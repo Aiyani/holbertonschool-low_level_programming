6cc2c6e188a0004d7318a6332f4e8f13b219db9e
Aiyani Williams <Aiyaniw@gmail.com>
2023-05-24 19:07:35 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts()
+# include<stdio.h>
+/**
+ *main - Prints "Programmingis like building a multilingual 
+ *               puzzle", followed by a new line
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
